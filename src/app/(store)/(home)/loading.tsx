a895e68563b4965a65e55cf68a12e92f637bf98c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-01-04 22:53:18 +0000

    add Skeleton Component

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,5 +1,11 @@
+import { Skeleton } from "@/components/skeleton";
+
 export default function HomeLoading(){
   return (
-    <p>Loading...</p>
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[856px]"/>
+      <Skeleton className="col-span-3 row-span-6"/>
+      <Skeleton className="col-span-3 row-span-6"/>
+    </div>
   )
 }
