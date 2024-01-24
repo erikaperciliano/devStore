d025c688853c996b568cc60b91d252bf961949a8
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-01-24 17:14:27 +0000

    creates search loading

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,11 +1,25 @@
-import { Skeleton } from "@/components/skeleton";
+'use client'
 
-export default function HomeLoading(){
+import { Skeleton } from "@/components/skeleton"
+import { useSearchParams } from "next/navigation"
+
+export default function SearchLoading(){
+    const searchParams = useSearchParams()
+
+    const query = searchParams.get('q')
+
   return (
-    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
-      <Skeleton className="col-span-6 row-span-6 h-[856px]"/>
-      <Skeleton className="col-span-3 row-span-6"/>
-      <Skeleton className="col-span-3 row-span-6"/>
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">Results for: <span className="font-semibold">{query}</span></p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[400px"/>
+        <Skeleton className="h-[400px"/>
+        <Skeleton className="h-[400px"/>
+        <Skeleton className="h-[400px"/>
+        <Skeleton className="h-[400px"/>
+        <Skeleton className="h-[400px"/>
+      </div>
     </div>
   )
 }
